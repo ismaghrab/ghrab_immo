4d07d52c31b7f123196ceca758ae32df19dc44e9
GHRAB ISMAIL p1801758 <ismail.ghrab@etu.univ-lyon1.fr>
2021-05-05 19:49:49 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+class HomeController extends AbstractController
+{
+
+    public function index():Response
+    {
+        return $this->render('pages/home.html.twig');
+        // return new Response("salut");
+    }
+}
